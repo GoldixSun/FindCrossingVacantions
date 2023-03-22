d4a2cdf180332d0f56da30e8a63bfd20ad6974f6
GoldixSun <goldixsun@gmail.com>
2023-03-22 17:09:47 +0500

    first commit

---
+++ FindCrossingVacantions/CrossVacantion.cs
@@ -0,0 +1,10 @@
+﻿
+
+namespace FindCrossingVacantions
+{
+    class CrossVacantion
+    {
+        public UserVacantion Vacantion { get; set; }
+        public UserVacantion CrossUserVacantion { get; set; }
+    }
+}
---
+++ FindCrossingVacantions/UserVacantion.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace FindCrossingVacantions
+{
+    class UserVacantion
+    {
+        public int UserID { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+    }
+}
